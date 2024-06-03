332bb95ed84bab8d4643711bb571404bd7c24cf0
bielbarros <gabrielbarrosg11@gmail.com>
2024-06-03 19:20:54 -0300

    adc aula de variaveís

--- src/SintaxeJava.java
+++ src/SintaxeJava.java
@@ -1,4 +1,19 @@
-public class SintaxeJava {
+    public class SintaxeJava {
+        public static void main (String[] args) {
+            /*
+            String meuNome = Gabriel;
+            byte idade = 123;
+            short ano = 2024;
+            int cep = 232532535; // se começar com 0 troque para string por conta da compilação.
+            long cpf = 83535345376L; // se começar com '0' troque para string por conta da compilação.
+            float pi = 3.14F;
+            double salario = 1275.33;
 
 
+
+            // Para variavel imutável usar o ("final" tipoDaVariavel NOME_DA_VARIAVEL = valorAtribuido)
+
+
+             */
+        }
 }
